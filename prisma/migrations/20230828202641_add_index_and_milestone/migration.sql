0066006dca59b63a6fa7f6ddbdce61657484757e
Dima Grossman <dima@grossman.io>
2023-09-29 11:23:05 +0300

    Delete migration.sql

--- prisma/migrations/20230828202641_add_index_and_milestone/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[githubUserId,linearUserId,githubRepoId,linearTeamId]` on the table `syncs` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "syncs_githubUserId_linearUserId_githubRepoId_linearTeamId_key" ON "syncs"("githubUserId", "linearUserId", "githubRepoId", "linearTeamId");
